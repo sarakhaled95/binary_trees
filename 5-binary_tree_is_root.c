8e63f184a159b9716fc4bb06984767378d9c5bcb
sarakhaled95 <architectsarakhaled@outlook.com>
2023-11-28 05:01:27 -0800

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,7 +7,9 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent == NULL)
+	if (node == NULL)
+		return (0);
+	else if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
